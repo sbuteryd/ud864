e159c7f5044b2776c81fa2cf1f3f54090ac73d75
li <russell@lideMacBook-Pro.local>
2018-10-09 14:40:19 +0800

    add map and api to html

--- js/map.js
+++ js/map.js
@@ -1,10 +1,6 @@
 function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-   let map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 47.6061111, lng: -122.4043043},
-        zoom: 13
-    });
+    let map = new google.maps.Map(document.getElementById('map'),{
+        center: {lat: 40.7413549, lng: -73.9980244},
+        zoom:13
+    })
 }
-
-
-console.log('this is initMap')
