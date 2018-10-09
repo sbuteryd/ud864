8e7c0bb68fcf82a5b588971e31195592d850edfb
li <russell@lideMacBook-Pro.local>
2018-10-09 14:01:37 +0800

    change center,the center for url

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
-   const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7413549, lng: -73.9980244},
+   let map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 47.6061111, lng: -122.4043043},
         zoom: 13
     });
 }
