a3959717b895bf0444b7507434c3198e0aa12277
li <russell@lideMacBook-Pro.local>
2018-10-09 14:51:49 +0800

    add marker

--- js/map.js
+++ js/map.js
@@ -2,5 +2,13 @@ function initMap() {
     let map = new google.maps.Map(document.getElementById('map'),{
         center: {lat: 40.7413549, lng: -73.9980244},
         zoom:13
-    })
+    });
+
+    let tribece = {lat:40.708294,lng: -73.999445}
+    let marker = new google.maps.Marker({
+        position:tribece,
+        map:map,
+        title:'fist marker'
+    });
+
 }
