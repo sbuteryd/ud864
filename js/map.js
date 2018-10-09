e176ab830ec93d4562efdbf6e218e22c8f463b6b
li <russell@lideMacBook-Pro.local>
2018-10-09 18:14:55 +0800

    recite mark

--- js/map.js
+++ js/map.js
@@ -1,21 +1,22 @@
 function initMap() {
     let map = new google.maps.Map(document.getElementById('map'),{
         center: {lat: 40.7413549, lng: -73.9980244},
-        zoom:13
+        zoom: 13
     });
 
-    let tribece = {lat:40.708294,lng: -73.999445};
-    let marker = new google.maps.Marker({
-        position:tribece,
+    let mark = new google.maps.Marker({
+        position:{lat:40.708076,lng: -73.999441},
         map:map,
-        title:'fist marker'
-    });
-
-    let infowindow =  new google.maps.InfoWindow({
-        content:'Do you ever feel like  this is Russell'
-    });
-
-    marker.addListener('click',function () {
-        infowindow.open(map,marker)
+        title:'russell'
     })
+    let inforMation = new google.maps.InfoWindow({
+        content:"russell"
+    })
+
+    mark.addListener('click',function () {
+        inforMation.open(map,mark)
+    })
+
 }
+
+initMap()
