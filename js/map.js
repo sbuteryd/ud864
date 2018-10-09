c595f9c1b37ba107980ec08d213af90b32afbfdc
li <russell@lideMacBook-Pro.local>
2018-10-09 12:00:17 +0800

    learn first

---
+++ js/map.js
@@ -0,0 +1,15 @@
+// function initMap() {
+//    let map = new google.maps.Map(document.getElementById('map'),{
+//         center:{lat:40.7413549,lng:-73.9980244},
+//         zoom:13
+//     })
+// }
+
+var map;
+function initMap() {
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.7413549, lng: -73.9980244},
+        zoom: 13
+    });
+}
