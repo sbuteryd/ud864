4615366024470233ab762a7f5dd7322c086d5d27
li <russell@lideMacBook-Pro.local>
2018-10-11 10:50:04 +0800

    add marker to map and chapter two

--- js/first.js
+++ js/first.js
@@ -4,6 +4,13 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'),{
         center:{lat: 40.712775,lng:-74.0059739},
         zoom:13
+    });
+    let place = {lat:40.693450,lng:-73.975847,};
+
+    let marker = new google.maps.Marker({
+        map:map,
+        position:place,
+        title:'practice'
     })
 }
 
