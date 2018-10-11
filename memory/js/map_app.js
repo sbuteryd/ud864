00310f44b5bb560a42b3204e9938a6cd4d5ac431
li <russell@lideMacBook-Pro.local>
2018-10-11 11:49:08 +0800

    review chaper1

---
+++ memory/js/map_app.js
@@ -0,0 +1,10 @@
+let map;
+//这个函数和api callback 设置的一样
+//=3&callback=initMap"
+function initMap() {
+    map = google.maps.Map(document.getElementById('map'),{
+        //经纬度
+        center:{lat:40.712775,lng:-74.005973},
+        zoom:13
+    })
+}
