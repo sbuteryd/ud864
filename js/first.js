795649eaf7c26240e67234801079c534f3b6f49c
li <russell@lideMacBook-Pro.local>
2018-10-11 10:15:38 +0800

    change lat lng and && Chapter one

--- js/first.js
+++ js/first.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'),{
-        center:{lat:40.6207833,lng:-74.7385159},
+        center:{lat: 40.712775,lng:-74.0059739},
         zoom:13
     })
 }
