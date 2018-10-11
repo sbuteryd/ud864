f30bced952183a045ee06f68722e48e7b1cee173
li <russell@lideMacBook-Pro.local>
2018-10-11 11:51:13 +0800

    fix bug and the map don't display

--- memory/js/map_app.js
+++ memory/js/map_app.js
@@ -2,7 +2,7 @@ let map;
 //这个函数和api callback 设置的一样
 //=3&callback=initMap"
 function initMap() {
-    map = google.maps.Map(document.getElementById('map'),{
+    map = new google.maps.Map(document.getElementById('map'),{
         //经纬度
         center:{lat:40.712775,lng:-74.005973},
         zoom:13
